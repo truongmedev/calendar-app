49899e0211a6e8a72a21b0d42b8c00b9739bab6a
truong.ngo <truongme.dev@gmail.com>
2026-09-12 13:15:17 +0700

    feat: init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-400">
+        Tailwind + CRA + TypeScript 🚀
+      </h1>
     </div>
   );
 }
